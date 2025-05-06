537fd3e6e0706be4ac4e7c6f301f56a10ec0672a
valentinalencioni <143528811+valentinalencioni@users.noreply.github.com>
2025-05-06 18:50:51 -0300

    tareas pendientes

--- src/app/tareas-pendientes/tareas-pendientes.page.ts
+++ src/app/tareas-pendientes/tareas-pendientes.page.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCheckbox, IonItem, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tareas-pendientes',
   templateUrl: './tareas-pendientes.page.html',
   styleUrls: ['./tareas-pendientes.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCheckbox, IonItem, IonList]
 })
-export class TareasPendientesPage implements OnInit {
+export class TareasPendientesPage {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  tareas = [
+    { descripcion: 'Diseñar interfaces responsivas', completada: false },
+    { descripcion: 'Implementar componentes reutilizables', completada: false },
+    { descripcion: 'Conectar API REST', completada: false },
+    { descripcion: 'Escribir pruebas unitarias', completada: false },
+    { descripcion: 'Depurar errores en el navegador', completada: false },
+  ];
 }
